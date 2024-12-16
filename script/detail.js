f43d9f80455322d48b299cd09e6eabfab69c9ab9
Arya Wiratama <aryawiratama2401@gmail.com>
2024-12-16 09:39:54 +0700

    edit

--- script/detail.js
+++ script/detail.js
@@ -11,3 +11,30 @@ ScrollReveal().reveal(".room .detail-img", {
     duration: 1000,
     reset: true,
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const roomDetailImgs = document.querySelectorAll(".imgClick");
+    const room = document.querySelector(".room"); 
+    const popup = document.getElementById("popup");
+    const popupImg = popup.querySelector("img");
+
+    roomDetailImgs.forEach(img => {
+        img.addEventListener("click", () => {
+            popupImg.src = img.src;
+
+            room.classList.toggle('active');
+            popup.classList.toggle('active');
+        });
+    });
+
+    document.addEventListener("click", (event) => {
+        if (!popup.contains(event.target) && !event.target.closest('.imgClick')) {
+            popup.classList.remove('active');
+            room.classList.remove('active');
+        }
+    });
+    
+});
+
+
+
